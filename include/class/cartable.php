c9a7255c00483a89fcfb67c4ddf410f95adda7c2
Atefeh Esmaili <atefeh.esmaili93@gmail.com>
2019-12-10 11:59:10 +0330

    update

--- include/class/cartable.php
+++ include/class/cartable.php
@@ -485,48 +485,7 @@ class Cartable{
                     <td><?php echo per_number(str_replace("-", "/", $row['si_admin_date'])); ?></td>
                     <td><?php echo get_user_name($row['si_writer']); ?></td>
 					<td>
-						<button class="btn btn-warning btn-xs" type="button" data-toggle="modal" data-keyboard="false" data-target="#admin_verify<?php echo  $row['si_id']; ?>" >تایید مدیر</button>
-							<div class="modal fade text-xs-left" id="admin_verify<?php echo $row['si_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="#admin_verify<?php echo $row['si_id']; ?>" style="display: none;" aria-hidden="true">
-								<div class="modal-dialog" role="document">
-									<form action="" method="post" enctype="multipart/form-data">
-										<input class="hidden" type="text" name="si_id" id="si_id" value="<?php echo $row['si_id']; ?>">
-										<div class="modal-content">
-											<div class="modal-header">
-												<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-													<span aria-hidden="true">×</span>
-												</button>
-												<h4 class="modal-title" id="myModalLabel3">تایید مدیر</h4>
-											</div>
-											<div class="modal-body">
-												<div class="row">
-													<div class="item col-md-12">
-														<div class="margin-tb input-group-prepend">
-															<span class="input-group-text">توضیحات مدیر</span>
-														</div>
-														<input type="text" id="si_admin_details" name="si_admin_details" placeholder="توضیحات مدیر" value="<?php echo per_number($row['si_admin_details']); ?>">
-													</div>
-												</div>
-												<div class="row">
-													<div class="item col-md-6">
-														<div class="margin-tb input-group-prepend">
-															<span class="input-group-text">وضعیت</span>
-														</div>
-														<select class="form-control" name="verify" id="verify">
-															<option value="<?php $u_id = $_SESSION['user_id']; echo $u_id; ?>">تایید</option>
-															<option value="0">عدم تایید</option>
-														</select>
-													</div>
-												</div>
-											</div>
-											<div class="modal-footer">
-												<button type="button" class="btn btn-danger btn-sm" data-dismiss="modal">انصراف</button>
-												<button class="btn btn-primary btn-sm" name="save_admin_verify" type="submit">ذخیره</button>
-											</div>
-										</div>
-									</form>
-								</div>
-							</div>
-						<a class="btn btn-success btn-xs" href="<?php get_url(); ?>secretariat/write_letter.php?si_id=<?php echo $row['si_id']; ?>&letter_type=<?php echo $row['si_type']; ?>">چاپ نامه</a>
+						<a class="btn btn-success btn-xs" href="<?php get_url(); ?>secretariat/write_letter.php?si_id=<?php echo $row['si_id']; ?>&letter_type=<?php echo $row['si_type']; ?>">مشاهده نامه</a>
 					</td>
 				</tr>
 				<?php
